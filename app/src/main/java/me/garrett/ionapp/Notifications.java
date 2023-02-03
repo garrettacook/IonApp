50342d36a2cf4f044336c13746abd06c1c395f43
Garrett Cook <garrettalexcook@gmail.com>
2023-02-03 14:00:28 -0500

    Link to website in notifications

--- app/src/main/java/me/garrett/ionapp/Notifications.java
+++ app/src/main/java/me/garrett/ionapp/Notifications.java
@@ -1,6 +1,9 @@
 package me.garrett.ionapp;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -20,12 +23,18 @@ public final class Notifications {
             ANNOUNCEMENTS_CHANNEL_ID = "me.garrett.ionapp.ANNOUNCEMENTS";
     public static final int BUS_NOTIFICATION_ID = 100, EIGHTH_ACTIVITY_ID = 101;
 
+    public static @NonNull PendingIntent getWebLinkPendingIntent(@NonNull Context context, @NonNull String path) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(IonUtils.BASE_URL + path));
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+    }
+
     public static void sendBusArrivalNotification(@NonNull Context context, @NonNull String route, @NonNull String locationMessage) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, BUS_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(context.getString(R.string.bus_arrived, route))
                 .setContentText(context.getString(R.string.generic_bus_status, locationMessage))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(getWebLinkPendingIntent(context, "bus"));
         NotificationManagerCompat.from(context).notify(BUS_NOTIFICATION_ID, builder.build());
     }
 
@@ -43,7 +52,8 @@ public final class Notifications {
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(activity)
                 .setContentText(context.getString(R.string.activity_location, room))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(getWebLinkPendingIntent(context, "eighth/glance"));
         NotificationManagerCompat.from(context).notify(EIGHTH_ACTIVITY_ID, builder.build());
     }
 
@@ -52,7 +62,8 @@ public final class Notifications {
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(announcement.getTitle())
                 .setContentText(announcement.getContent())
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(getWebLinkPendingIntent(context, "announcements/" + announcement.getId()));
         NotificationManagerCompat.from(context).notify(announcement.getId(), builder.build());
     }
 
